81286142c45268e723793f36d22386b8fa2dc71d
Matthieu Petiot <1927154+ardeidae@users.noreply.github.com>
2021-12-14 23:06:21 +0100

    Fix install script warnings

--- .mydotfiles-misc/install-linux-debian.sh
+++ .mydotfiles-misc/install-linux-debian.sh
@@ -1,4 +1,4 @@
-#/bin/bash
+#!/bin/bash
 
 # uncomment to debug
 # set -x
@@ -42,6 +42,6 @@ sudo apt install gh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # install zsh-syntax-highlighting plugin
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting"
 # install zsh-history-substring-search plugin
-git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
+git clone https://github.com/zsh-users/zsh-history-substring-search "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search"
