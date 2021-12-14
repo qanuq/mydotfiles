41cba3fddd3e1e1682bae207adb1cd65e20ed967
Matthieu Petiot <1927154+ardeidae@users.noreply.github.com>
2021-12-14 15:45:00 +0100

    Add neovim on debian

--- .mydotfiles-misc/install-linux-debian.sh
+++ .mydotfiles-misc/install-linux-debian.sh
@@ -21,6 +21,7 @@ sudo apt install -y htop
 sudo apt install -y keychain
 sudo apt install -y mosh
 sudo apt install -y ncdu
+sudo apt install -y neovim
 
 curl --silent --location https://raw.githubusercontent.com/petervanderdoes/gitflow-avh/develop/contrib/gitflow-installer.sh --output ./gitflow-installer.sh && sudo /bin/bash gitflow-installer.sh install stable; rm -fv gitflow-installer.sh
 
