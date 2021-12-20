d0fb6b3beb67b2ab80904c77ed8b1622eb31daa9
Matthieu Petiot <1927154+ardeidae@users.noreply.github.com>
2021-12-20 22:46:34 +0100

    Fix bash script warning

--- .mydotfiles-misc/install-macos.sh
+++ .mydotfiles-misc/install-macos.sh
@@ -365,6 +365,6 @@ killall SystemUIServer
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # install zsh-syntax-highlighting plugin
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting"
 # install zsh-history-substring-search plugin
-git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
+git clone https://github.com/zsh-users/zsh-history-substring-search "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search"
