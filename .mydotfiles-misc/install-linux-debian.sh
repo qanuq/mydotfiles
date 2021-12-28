b607008f2a6cf3c030eba0608599d4547bbf2c18
Matthieu Petiot <1927154+ardeidae@users.noreply.github.com>
2021-12-28 14:15:31 +0100

    Add -y arg on apt install

--- .mydotfiles-misc/install-linux-debian.sh
+++ .mydotfiles-misc/install-linux-debian.sh
@@ -64,7 +64,7 @@ curl --silent --location https://raw.githubusercontent.com/petervanderdoes/gitfl
 curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
 sudo apt update
-sudo apt install gh
+sudo apt install -y gh
 
 # install ohmyzsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
