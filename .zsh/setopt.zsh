3f6aba8b2c9b951c8cd678b9215e3dbcc5d70daa
Matthieu Petiot <1927154+ardeidae@users.noreply.github.com>
2024-03-29 15:43:41 +0100

    Revert back some options

--- .zsh/setopt.zsh
+++ .zsh/setopt.zsh
@@ -4,8 +4,10 @@
 # http://zsh.sourceforge.net/Doc/Release/Options.html#Changing-Directories
 setopt auto_cd                 # if a command isn't valid, but is a directory, cd to that dir
 setopt auto_pushd              # make cd push the old directory onto the directory stack
+setopt cd_silent               # print nothing when typing cd -
 setopt pushd_ignore_dups       # don’t push multiple copies of the same directory onto the directory stack
 setopt pushd_minus             # exchanges the meanings of ‘+’ and ‘-’ when specifying a directory in the stack
+setopt pushd_silent            # print nothing when typing pushd
 setopt pushd_to_home           # pushd without args go to home
 
 # Completions
