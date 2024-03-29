a1358063ac95c6eb732b7d50e5977fca8edb0b47
Matthieu Petiot <1927154+ardeidae@users.noreply.github.com>
2024-03-29 14:47:16 +0100

    Refactor direnv install

--- .mydotfiles-install/install-direnv.sh
+++ .mydotfiles-install/install-direnv.sh
@@ -3,27 +3,16 @@
 # uncomment to debug
 # set -x
 
-release="latest"
 kernel=$(uname -s | tr "[:upper:]" "[:lower:]")
 case "$(uname -m)" in
-    x86_64)
-        machine=amd64
-        ;;
-    i686 | i386)
-        machine=386
-        ;;
-    armv7l)
-        machine=arm
-        ;;
-    aarch64 | arm64)
-        machine=arm64
-        ;;
-    *)
-        ;;
+    x86_64) machine=amd64 ;;
+    i686 | i386) machine=386 ;;
+    armv7l) machine=arm ;;
+    aarch64 | arm64) machine=arm64 ;;
 esac
 
 download_url=$(
-curl -sL "https://api.github.com/repos/direnv/direnv/releases/$release" \
+curl -sL "https://api.github.com/repos/direnv/direnv/releases/latest" \
     | grep browser_download_url \
     | cut -d '"' -f 4 \
     | grep "direnv.$kernel.$machine\$"
