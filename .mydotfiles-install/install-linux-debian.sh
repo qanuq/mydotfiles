efa935ad345ea901c813f1417e899c3d40e28779
Matthieu Petiot <1927154+ardeidae@users.noreply.github.com>
2024-04-22 17:56:31 +0200

    Remove full command paths

--- .mydotfiles-install/install-linux-debian.sh
+++ .mydotfiles-install/install-linux-debian.sh
@@ -6,33 +6,33 @@
 script_dir=$(dirname "$(realpath "$0")")
 
 # ask for the administrator password upfront
-/usr/bin/sudo -v
+sudo -v
 
 # keep-alive: update existing `sudo` time stamp until this script has finished
-while true; do /usr/bin/sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 # add sudoedit link
 [ ! -e /usr/local/bin/sudoedit ] && ln -s /usr/bin/sudo /usr/local/bin/sudoedit
 
 mkdir -p "$HOME/.local/bin"
 
-/usr/bin/sudo /usr/bin/apt update
+sudo apt update
 
 # allow gitconfig interactive.singleKey to work
-/usr/bin/sudo /usr/bin/apt install -y libterm-readkey-perl
+sudo apt install -y libterm-readkey-perl
 
-/usr/bin/sudo /usr/bin/apt install -y bat
-/usr/bin/sudo /usr/bin/apt install -y fd-find
-/usr/bin/sudo /usr/bin/apt install -y ripgrep
-/usr/bin/sudo /usr/bin/apt install -y tig
-/usr/bin/sudo /usr/bin/apt install -y tmux
+sudo apt install -y bat
+sudo apt install -y fd-find
+sudo apt install -y ripgrep
+sudo apt install -y tig
+sudo apt install -y tmux
 
-/usr/bin/sudo /usr/bin/apt install -y zsh
+sudo apt install -y zsh
 
-/usr/bin/sudo /usr/bin/dpkg-divert --local --divert /usr/bin/bat --rename --add /usr/bin/batcat
-/usr/bin/sudo /usr/bin/dpkg-divert --local --divert /usr/bin/fd --rename --add /usr/bin/fdfind
+sudo dpkg-divert --local --divert /usr/bin/bat --rename --add /usr/bin/batcat
+sudo dpkg-divert --local --divert /usr/bin/fd --rename --add /usr/bin/fdfind
 
-/usr/bin/sudo update-alternatives --set editor /usr/bin/vim.basic
+sudo update-alternatives --set editor /usr/bin/vim.basic
 
 # install eza
 sudo apt install -y gpg
