59fa0fec3e0a0568e750c3b5f36bf96502d7be8f
Matthieu Petiot <1927154+ardeidae@users.noreply.github.com>
2024-08-02 23:56:32 +0200

    Add which-key

---
+++ .config/nvim/lua/qanuq/plugins/which-key.lua
@@ -0,0 +1,10 @@
+return {
+  "folke/which-key.nvim",
+  event = "VeryLazy",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  opts = {
+    delay = function()
+      return 500
+    end,
+  },
+}
