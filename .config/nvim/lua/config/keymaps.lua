d693fcfe0fc1e6d4f0c11a7b61b3c09d6e904824
Matthieu Petiot <1927154+ardeidae@users.noreply.github.com>
2024-08-03 23:05:01 +0200

    Add keymaps

---
+++ .config/nvim/lua/config/keymaps.lua
@@ -0,0 +1,39 @@
+local keymap = vim.keymap
+
+vim.g.mapleader = " "
+vim.g.maplocalleader = ","
+
+keymap.set("i", "jj", "<ESC>", { desc = "Exit insert mode with jj" })
+
+keymap.set("n", "<leader>nh", ":noh<CR>", { desc = "Clear search highlights" })
+
+-- increment/decrement numbers
+keymap.set("n", "<leader>+", "<C-a>", { desc = "Increment number" })
+keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement number" })
+
+-- window management
+keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" })
+keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" })
+keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" })
+keymap.set("n", "<leader>sx", ":close<CR>", { desc = "Close current split" })
+
+-- tab management
+keymap.set("n", "<leader>to", ":tabnew<CR>", { desc = "Open new tab" })
+keymap.set("n", "<leader>tx", ":tabclose<CR>", { desc = "Close current tab" })
+keymap.set("n", "<leader>tp", ":tabp<CR>", { desc = "Go to previous tab" })
+keymap.set("n", "<leader>tn", ":tabn<CR>", { desc = "Go to next tab" })
+keymap.set("n", "<leader>tf", ":tabnew %<CR>", { desc = "Open current buffer in new tab" })
+
+-- buffers management
+vim.api.nvim_set_keymap("n", "<leader>bf", ":bfirst<CR>", { desc = "Go to first buffer" })
+vim.api.nvim_set_keymap("n", "<leader>bl", ":blast<CR>", { desc = "Go to last buffer" })
+vim.api.nvim_set_keymap("n", "<leader>bp", ":bprev<CR>", { desc = "Go to previous buffer" })
+vim.api.nvim_set_keymap("n", "<leader>bn", ":bnext<CR>", { desc = "Go to next buffer" })
+vim.api.nvim_set_keymap("n", "<leader>bd", ":bdelete<CR>", { desc = "Close current buffer" })
+
+-- remap for dealing with line wrap.
+keymap.set("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+keymap.set("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+
+-- do nothing with space
+keymap.set({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -1,4 +1,3 @@
--- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = "https://github.com/folke/lazy.nvim.git"
@@ -15,21 +14,10 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
--- Make sure to setup `mapleader` and `maplocalleader` before
--- loading lazy.nvim so that mappings are correct.
--- This is also a good place to setup other settings (vim.opt)
-vim.g.mapleader = " "
-vim.g.maplocalleader = ","
-
--- Setup lazy.nvim
 require("lazy").setup({
   spec = {
-    -- import your plugins
     { import = "config.plugins" },
   },
-  -- Configure any other settings here. See the documentation for more details.
-  -- automatically check for plugin updates
   checker = { enabled = true, notify = false },
-  -- automatically check for config file changes and reload the ui
   change_detection = { enabled = true, notify = true },
 })
