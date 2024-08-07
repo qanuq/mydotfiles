db121357a64ee8bf205671bcb0a2b10f69ed0acd
Matthieu Petiot <1927154+ardeidae@users.noreply.github.com>
2024-08-08 01:16:17 +0200

    Set github theme in diff mode

--- .config/nvim/lua/config/plugins/colorscheme.lua
+++ .config/nvim/lua/config/plugins/colorscheme.lua
@@ -1,10 +1,16 @@
 return {
-  "folke/tokyonight.nvim",
-  priority = 1000,
-  config = function()
-    require("tokyonight").setup({
-      style = "night",
-    })
-    vim.cmd[[colorscheme tokyonight]]
-  end,
+  {
+    "folke/tokyonight.nvim",
+    priority = 1000,
+    config = function()
+      require("tokyonight").setup({
+        style = "night",
+      })
+      vim.cmd[[colorscheme tokyonight]]
+    end,
+  },
+  {
+    "projekt0n/github-nvim-theme",
+    priority = 900,
+  },
 }
