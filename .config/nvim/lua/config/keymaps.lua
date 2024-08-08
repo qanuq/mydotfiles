b866741ba57ba4a4df0f709478991ef12a92757b
Matthieu Petiot <1927154+ardeidae@users.noreply.github.com>
2024-08-08 11:40:51 +0200

    Use <Leader> and <Cmd> in keymaps

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -5,31 +5,31 @@ vim.g.maplocalleader = ","
 
 keymap.set("i", "jj", "<ESC>", { desc = "Exit insert mode with jj" })
 
-keymap.set("n", "<leader>nh", ":noh<Enter>", { desc = "Clear search highlights" })
+keymap.set("n", "<Leader>nh", "<Cmd>noh<Enter>", { desc = "Clear search highlights" })
 
 -- increment/decrement numbers
-keymap.set("n", "<leader>+", "<C-a>", { desc = "Increment number" })
-keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement number" })
+keymap.set("n", "<Leader>+", "<C-a>", { desc = "Increment number" })
+keymap.set("n", "<Leader>-", "<C-x>", { desc = "Decrement number" })
 
 -- window management
-keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" })
-keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" })
-keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" })
-keymap.set("n", "<leader>sx", ":close<Enter>", { desc = "Close current split" })
+keymap.set("n", "<Leader>sv", "<C-w>v", { desc = "Split window vertically" })
+keymap.set("n", "<Leader>sh", "<C-w>s", { desc = "Split window horizontally" })
+keymap.set("n", "<Leader>se", "<C-w>=", { desc = "Make splits equal size" })
+keymap.set("n", "<Leader>sx", "<Cmd>close<Enter>", { desc = "Close current split" })
 
 -- tab management
-keymap.set("n", "<leader>to", ":tabnew<Enter>", { desc = "Open new tab" })
-keymap.set("n", "<leader>tx", ":tabclose<Enter>", { desc = "Close current tab" })
-keymap.set("n", "<leader>tp", ":tabp<Enter>", { desc = "Go to previous tab" })
-keymap.set("n", "<leader>tn", ":tabn<Enter>", { desc = "Go to next tab" })
-keymap.set("n", "<leader>tf", ":tabnew %<Enter>", { desc = "Open current buffer in new tab" })
+keymap.set("n", "<Leader>to", "<Cmd>tabnew<Enter>", { desc = "Open new tab" })
+keymap.set("n", "<Leader>tx", "<Cmd>tabclose<Enter>", { desc = "Close current tab" })
+keymap.set("n", "<Leader>tp", "<Cmd>tabp<Enter>", { desc = "Go to previous tab" })
+keymap.set("n", "<Leader>tn", "<Cmd>tabn<Enter>", { desc = "Go to next tab" })
+keymap.set("n", "<Leader>tf", "<Cmd>tabnew %<Enter>", { desc = "Open current buffer in new tab" })
 
 -- buffers management
-keymap.set("n", "<leader>bf", ":bfirst<Enter>", { desc = "Go to first buffer" })
-keymap.set("n", "<leader>bl", ":blast<Enter>", { desc = "Go to last buffer" })
-keymap.set("n", "<leader>bp", ":bprev<Enter>", { desc = "Go to previous buffer" })
-keymap.set("n", "<leader>bn", ":bnext<Enter>", { desc = "Go to next buffer" })
-keymap.set("n", "<leader>bd", ":bdelete<Enter>", { desc = "Close current buffer" })
+keymap.set("n", "<Leader>bf", "<Cmd>bfirst<Enter>", { desc = "Go to first buffer" })
+keymap.set("n", "<Leader>bl", "<Cmd>blast<Enter>", { desc = "Go to last buffer" })
+keymap.set("n", "<Leader>bp", "<Cmd>bprev<Enter>", { desc = "Go to previous buffer" })
+keymap.set("n", "<Leader>bn", "<Cmd>bnext<Enter>", { desc = "Go to next buffer" })
+keymap.set("n", "<Leader>bd", "<Cmd>bdelete<Enter>", { desc = "Close current buffer" })
 
 -- remap for dealing with line wrap.
 keymap.set("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
@@ -39,9 +39,10 @@ keymap.set("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true }
 keymap.set({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
 
 -- use F1 to invert list display instead of help
-keymap.set('n', '<F1>', ':set invlist<Enter>', { noremap = true, silent = true })
-keymap.set('i', '<F1>', '<C-o>:set invlist<Enter>', { noremap = true, silent = true })
+-- keymap.set('n', '<F1>', '<Cmd>set invlist<Enter>', { noremap = true, silent = true })
+keymap.set('n', '<F1>', '<Cmd>set invlist<Enter>', { noremap = true, silent = true })
+keymap.set('i', '<F1>', '<C-o><Cmd>set invlist<Enter>', { noremap = true, silent = true })
 
 -- use F2 to invert wrap
-keymap.set('n', '<F2>', ':set invwrap<Enter>', { noremap = true, silent = true })
-keymap.set('i', '<F2>', '<C-o>:set invwrap<Enter>', { noremap = true, silent = true })
+keymap.set('n', '<F2>', '<Cmd>set invwrap<Enter>', { noremap = true, silent = true })
+keymap.set('i', '<F2>', '<C-o><Cmd>set invwrap<Enter>', { noremap = true, silent = true })
