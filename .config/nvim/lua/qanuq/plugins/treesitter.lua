50ded30c593a1d4bbc0ebdfd3fe058a2e8ee5943
Matthieu Petiot <1927154+ardeidae@users.noreply.github.com>
2024-08-17 00:02:23 +0200

    Add treesitter textobjects

--- .config/nvim/lua/qanuq/plugins/treesitter.lua
+++ .config/nvim/lua/qanuq/plugins/treesitter.lua
@@ -1,5 +1,8 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
+  "nvim-treesitter/nvim-treesitter-textobjects",
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter",
+  },
   event = { "BufReadPre", "BufNewFile" },
   build = ":TSUpdate",
   config = function()
@@ -13,6 +16,7 @@ return {
         "comment",
         "cpp",
         "css",
+        "diff",
         "dockerfile",
         "gitignore",
         "html",
@@ -48,6 +52,77 @@ return {
           node_incremental = "grn",
           scope_incremental = "grc",
           node_decremental = "grm",
+        },
+      },
+
+      textobjects = {
+        select = {
+          enable = true,
+
+          lookahead = true,
+
+          -- TODO: improve all mappings
+          keymaps = {
+            ["a="] = { query = "@assignment.outer", desc = "Select outer part of an assignment" },
+            ["i="] = { query = "@assignment.inner", desc = "Select inner part of an assignment" },
+            ["l="] = { query = "@assignment.lhs", desc = "Select left hand side of an assignment" },
+            ["r="] = { query = "@assignment.rhs", desc = "Select right hand side of an assignment" },
+
+            -- works for javascript/typescript files (custom capture I created in after/queries/ecma/textobjects.scm)
+            ["a:"] = { query = "@property.outer", desc = "Select outer part of an object property" },
+            ["i:"] = { query = "@property.inner", desc = "Select inner part of an object property" },
+            ["l:"] = { query = "@property.lhs", desc = "Select left part of an object property" },
+            ["r:"] = { query = "@property.rhs", desc = "Select right part of an object property" },
+
+            ["aa"] = { query = "@parameter.outer", desc = "Select outer part of a parameter/argument" },
+            ["ia"] = { query = "@parameter.inner", desc = "Select inner part of a parameter/argument" },
+
+            ["ai"] = { query = "@conditional.outer", desc = "Select outer part of a conditional" },
+            ["ii"] = { query = "@conditional.inner", desc = "Select inner part of a conditional" },
+
+            ["al"] = { query = "@loop.outer", desc = "Select outer part of a loop" },
+            ["il"] = { query = "@loop.inner", desc = "Select inner part of a loop" },
+
+            ["af"] = { query = "@call.outer", desc = "Select outer part of a function call" },
+            ["if"] = { query = "@call.inner", desc = "Select inner part of a function call" },
+
+            ["am"] = { query = "@function.outer", desc = "Select outer part of a method/function definition" },
+            ["im"] = { query = "@function.inner", desc = "Select inner part of a method/function definition" },
+
+            ["ac"] = { query = "@class.outer", desc = "Select outer part of a class" },
+            ["ic"] = { query = "@class.inner", desc = "Select inner part of a class" },
+          },
+        },
+        swap = {
+          enable = true,
+          swap_next = {
+            ["<leader>na"] = "@parameter.inner",  -- swap parameter with next
+            ["<leader>nf"] = "@function.outer",   -- swap function with next
+          },
+          swap_previous = {
+            ["<leader>pa"] = "@parameter.inner",  -- swap parameters with prev
+            ["<leader>pf"] = "@function.outer",   -- swap function with previous
+          },
+        },
+        move = {
+          enable = true,
+          set_jumps = true,
+          goto_next_start = {
+            [";f"] = { query = "@function.outer", desc = "Next function def start" },
+            [";c"] = { query = "@class.outer", desc = "Next class start" },
+          },
+          goto_next_end = {
+            [";F"] = { query = "@function.outer", desc = "Next function def end" },
+            [";C"] = { query = "@class.outer", desc = "Next class end" },
+          },
+          goto_previous_start = {
+            [",f"] = { query = "@function.outer", desc = "Prev function def start" },
+            [",c"] = { query = "@class.outer", desc = "Prev class start" },
+          },
+          goto_previous_end = {
+            [",F"] = { query = "@function.outer", desc = "Prev function def end" },
+            [",C"] = { query = "@class.outer", desc = "Prev class end" },
+          },
         },
       },
     })
