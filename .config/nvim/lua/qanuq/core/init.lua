4edd10d7165fb4a4d019a868f7e93dbc3520a6d8
Matthieu Petiot <1927154+ardeidae@users.noreply.github.com>
2024-08-19 15:39:45 +0200

    Disable some providers

--- .config/nvim/lua/qanuq/core/init.lua
+++ .config/nvim/lua/qanuq/core/init.lua
@@ -1,3 +1,4 @@
 require("qanuq.core.keymaps")
 require("qanuq.core.autocmd")
 require("qanuq.core.options")
+require("qanuq.core.misc")
---
+++ .config/nvim/lua/qanuq/core/misc.lua
@@ -0,0 +1,11 @@
+-- disable python provider
+vim.g.loaded_python3_provider = 0
+
+-- disable ruby provider
+vim.g.loaded_ruby_provider = 0
+
+-- disable perl provider
+vim.g.loaded_perl_provider = 0
+
+-- disable node provider
+vim.g.loaded_node_provider = 0
