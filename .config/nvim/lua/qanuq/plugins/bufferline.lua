28e6a5c7e3b5f4149f95b31d82026aeebf4856a2
Matthieu Petiot <1927154+ardeidae@users.noreply.github.com>
2024-08-27 17:04:02 +0200

    Improve bufferline

--- .config/nvim/lua/qanuq/plugins/bufferline.lua
+++ .config/nvim/lua/qanuq/plugins/bufferline.lua
@@ -2,10 +2,14 @@ return {
   "akinsho/bufferline.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
   version = "*",
-  opts = {
-    options = {
-      mode = "buffers",
-      separator_style = "thin",
+    opts = {
+      options = {
+        -- TODO: diagnostic and mappings
+        show_close_icon = false,
+        show_buffer_close_icons = false,
+        mode = "buffers",
+        separator_style = "thin",
+        numbers = "ordinal",
+      },
     },
-  },
 }
